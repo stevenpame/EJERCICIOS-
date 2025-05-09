476a4821814bf2131765afdcef18c8cb25663b76
stevenpame <dubanestevenfarinangopame55@gmail.com>
2025-05-09 09:41:56 -0500

    javascrippuro

---
+++ JAVASCRIPTPURO/ejer0.js
@@ -0,0 +1,55 @@
+if(5>7){
+    console.log("verdadero")
+}else{
+    console.log("falso")
+}
+let dato="Cine"
+let estadotiempo="lloviendo"
+if (estadotiempo==="verano"){
+    console.log("voy a " + dato)
+}else{
+    console.log("me quedo en casa")
+}
+let x=10
+if (x>5){
+    console.log("Mayor")
+}
+{
+    let Z=20
+}
+
+let valor=5
+
+if (valor===1){
+    console.log("uno")
+}
+if (valor===2){
+    console.log("dos")
+}
+if (valor===3){
+    console.log("tres")
+}
+if (valor===4){
+    console.log("cuatro")
+}
+if (valor===5){
+    console.log("cinco")
+}
+
+if (valor|==1 && valor|==2  && valor|==3  && valor|==4 && valor|==5){
+    console.log(" direfente valor de 1 a 5 ")
+} 
+
+if (valor===1){
+    console.log("uno")
+}else if(valor===2){
+    console.log("dos")
+}else if (valor===3){
+    console.log("tres")
+}else if (valor===4){
+    console.log("cuatro")
+}else if (valor===5){
+    console.log("cinco")
+}else{
+    console.log("otro valor")
+}
